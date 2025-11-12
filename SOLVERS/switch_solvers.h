77cea12e99cf69b7a7aaa6a43028fd4e9a0e4828
Sahaj Sunil Jain <67571850+SahajSJain@users.noreply.github.com>
2025-11-11 19:49:16 -0500

    Delete SOLVERS directory

--- SOLVERS/switch_solvers.h
+++
@@ -1,8 +0,0 @@
-#ifndef SWITCH_SOLVERS_H
-#define SWITCH_SOLVERS_H
-
-#include "../INCLUDE/structs.cuh"
-
-void switch_solver(Solver &Sol, Setup *setup, real_t *time_total, real_t *time_iteration, int *num_iterations);
-
-#endif // SWITCH_SOLVERS_H
