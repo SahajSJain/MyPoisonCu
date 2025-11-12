7fa7410c06b89d4de9e5dcd53a075b809b572387
Sahaj Sunil Jain <67571850+SahajSJain@users.noreply.github.com>
2025-11-11 19:49:04 -0500

    Delete INCLUDE directory

--- INCLUDE/field_instantiate.cpp
+++
@@ -1,8 +0,0 @@
-// INCLUDE/field_instantiate.cpp
-#include "structs.cuh"
-
-// Include the implementation file so the compiler can see the template definitions
-#include "field.cpp"
-#include "field.cu"
-
-
